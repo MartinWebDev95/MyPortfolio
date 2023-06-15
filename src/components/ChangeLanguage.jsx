9ed1bb10a5da9d526af3b1d9a6477e96e21799f7
Juan Antonio <jamll.muro@gmail.com>
2023-06-15 13:27:14 +0200

    Fix a problem with the overflow of ChangeLanguage component

--- src/components/ChangeLanguage.jsx
+++ src/components/ChangeLanguage.jsx
@@ -3,21 +3,46 @@ import { useState } from 'react';
 function ChangeLanguage() {
   const [isVisible, setIsVisible] = useState(true);
 
+  const handleLanguage = (e) => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <footer className={`absolute bottom-0 left-0 dark:bg-black/30 bg-white/30 w-full shadow-lg flex z-10 ${isVisible ? 'h-20' : 'h-0'} transition-all duration-300 ease-in-out`}>
+    <footer className={`fixed bottom-0 left-0 dark:bg-black/30 bg-white/30 w-full shadow-lg flex z-10 ${isVisible ? 'h-20' : 'h-0'} transition-all duration-300 ease-in-out`}>
+
       <button
         type="button"
-        className={`text-pink-700 font-bold absolute right-1/2 ${isVisible ? 'bottom-20 bg-[url(/assets/arrow-down.png)]' : 'bottom-0 bg-[url(/assets/arrow-up.png)]'} dark:bg-black/30 bg-white/30 w-16 h-8 z-0 text-xl rounded-t-full bg-no-repeat bg-center bg-contain transition-all duration-300 ease-in-out`}
+        className={`text-pink-700 font-bold absolute right-1/2 flex items-center justify-center ${isVisible ? 'bottom-20' : 'bottom-0'} dark:bg-black/30 bg-white/30 w-14 h-8 rounded-t-full transition-all duration-300 ease-in-out group`}
         onClick={() => setIsVisible(!isVisible)}
-      />
+      >
+        {isVisible ? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#be185d" className="group-hover:fill-blue-600 transition-all duration-300 ease-in-out">
+            <polygon points="12 17.414 3.293 8.707 4.707 7.293 12 14.586 19.293 7.293 20.707 8.707 12 17.414" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#be185d" className="group-hover:fill-blue-600 transition-all duration-300 ease-in-out">
+            <polygon points="12 6.586 3.293 15.293 4.707 16.707 12 9.414 19.293 16.707 20.707 15.293 12 6.586" />
+          </svg>
+        )}
+      </button>
 
-      <div className={`container mx-auto w-full ${isVisible ? 'flex items-center justify-center gap-2' : 'hidden'}`}>
+      <div className={`container mx-auto w-full ${isVisible ? 'flex items-center justify-center gap-2' : 'hidden'} overflow-hidden`}>
         <p className="text-pink-700 font-bold">Select the language: </p>
 
         <div className="flex gap-4 items-center">
-          <button type="button" className="bg-[url('/assets/spain-flag.png')] w-10 h-10 bg-no-repeat bg-cover bg-center" />
+          <button
+            type="button"
+            value="es"
+            className="bg-[url('/assets/spain-flag.png')] w-10 h-10 bg-no-repeat bg-cover bg-center"
+            onClick={handleLanguage}
+          />
 
-          <button type="button" className="bg-[url('/assets/usa-flag.png')] w-10 h-10 bg-no-repeat bg-cover bg-center" />
+          <button
+            type="button"
+            value="en"
+            className="bg-[url('/assets/usa-flag.png')] w-10 h-10 bg-no-repeat bg-cover bg-center"
+            onClick={handleLanguage}
+          />
         </div>
       </div>
 
