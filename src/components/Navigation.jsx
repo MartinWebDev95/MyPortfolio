eb433723d1b924f6a390c7457087dd42ee13b5a7
Juan Antonio <jamll.muro@gmail.com>
2023-06-16 14:20:06 +0200

    Change the navigation background opacity

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 function Navigation({ isMenuOpen }) {
   return (
-    <nav className={`absolute md:static bg-white/50 dark:bg-black/50 top-0 left-0 w-full ${isMenuOpen ? 'h-[calc(100vh-5rem)]' : 'h-0'} overflow-hidden md:w-auto md:h-auto md:bg-transparent md:dark:bg-transparent mt-20 md:mt-0 transition-all duration-300 ease-in-out`}>
+    <nav className={`absolute md:static bg-white/95 dark:bg-black/95 top-0 left-0 w-full ${isMenuOpen ? 'h-[calc(100vh-5rem)]' : 'h-0'} overflow-hidden md:w-auto md:h-auto md:bg-transparent md:dark:bg-transparent mt-20 md:mt-0 transition-all duration-300 ease-in-out`}>
       <ul className="flex flex-col items-center md:flex-row gap-8 mt-4 md:mt-0">
         <li>
           <a
