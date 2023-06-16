38d3d30f1ca961df8aca3f4a1b4f91b0236f7d67
Juan Antonio <jamll.muro@gmail.com>
2023-06-16 13:33:04 +0200

    Change some styles in main label

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -10,9 +10,11 @@ function Portfolio() {
     <>
       <Header />
 
-      <main className="pt-20 container mx-auto px-4 md:px-0">
-        <section className="bg-white/20 dark:bg-black/20 rounded-lg shadow-lg mt-4">
-          <h2 className="text-center uppercase font-bold text-pink-700 text-2xl pt-4">Projects</h2>
+      <main className="container mx-auto px-4 md:px-0">
+        <section className="bg-white/20 dark:bg-black/20 rounded-lg shadow-lg my-4">
+          <h2 className="text-center uppercase font-bold text-pink-700 text-2xl pt-4">
+            Projects
+          </h2>
 
           <ListOfProjects projects={projects} />
         </section>
