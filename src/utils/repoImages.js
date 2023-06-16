59b6c50eac0151287a06ebf3e473940ddd220252
Juan Antonio <jamll.muro@gmail.com>
2023-06-16 09:43:52 +0200

    Add new object that add an image and title to the repo array

---
+++ src/utils/repoImages.js
@@ -0,0 +1,24 @@
+const repoImages = {
+  AudiophileEcommerce: {
+    image: '/images/audiophile.png',
+    title: 'Audiophile E-commerce',
+  },
+  DisneyPlusClone: {
+    image: '/images/disneyPlusClone.png',
+    title: 'Disney Plus Clone',
+  },
+  TrelloWeb: {
+    image: '/images/trelloWeb.png',
+    title: 'Trello Web',
+  },
+  Kanban: {
+    image: '/images/kanban.png',
+    title: 'Kanban Task Management',
+  },
+  StaticJobsListingReact: {
+    image: '/images/staticJobsListing.png',
+    title: 'Static Jobs Listing',
+  },
+};
+
+export default repoImages;
