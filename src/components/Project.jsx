38902dce5470220391ef0817dd761043c39a0fb2
Juan Antonio <jamll.muro@gmail.com>
2023-06-19 14:05:08 +0200

    Change some styles in Project component and add the technologies used in each project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,31 +1,48 @@
 function Project({ project }) {
   return (
     <div className="rounded-lg bg-white dark:bg-black shadow-lg">
-      <img src={project.repoImage} alt={project.repoTitle} className="rounded-t-lg" />
+      <div>
+        <img src={project.repoImage} alt={project.repoTitle} className="rounded-t-lg" />
+      </div>
 
-      <div className="p-4">
-        <h2 className="text-pink-700 text-base md:text-xl font-bold">
+      <div className="p-4 h-auto">
+        <h2 className="text-pink-700 text-base md:text-xl font-bold mb-4">
           {project.repoTitle}
         </h2>
 
-        <div className="flex items-center gap-4 mt-4">
-          <a
-            href={project.html_url}
-            target="_blank"
-            className="rounded-full bg-transparent border-2 border-pink-700 text-pink-700 py-1 px-3 hover:bg-blue-600 hover:text-white hover:border-white dark:hover:border-black transition-all duration-300 ease-in-out"
-            rel="noreferrer noopener"
-          >
-            Github
-          </a>
+        <p className="text-pink-700">{project.description}</p>
 
-          <a
-            href={project.homepage}
-            target="_blank"
-            className="rounded-full bg-transparent border-2 border-pink-700 text-pink-700 py-1 px-3 hover:bg-blue-600 hover:text-white hover:border-white dark:hover:border-black transition-all duration-300 ease-in-out"
-            rel="noreferrer noopener"
-          >
-            Live Demo
-          </a>
+        <div className="flex flex-col flex-end">
+          <ul className="flex flex-wrap gap-2 my-4">
+            {project.topics.map((topic) => (
+              <li
+                key={topic}
+                className="bg-pink-700 text-white dark:text-black rounded-full w-fit px-2"
+              >
+                {topic}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex items-center gap-4">
+            <a
+              href={project.html_url}
+              target="_blank"
+              className="rounded-full bg-transparent border-2 border-pink-700 text-pink-700 py-1 px-3 hover:bg-blue-600 hover:text-white dark:hover:text-black hover:border-white dark:hover:border-black transition-all duration-300 ease-in-out"
+              rel="noreferrer noopener"
+            >
+              Github
+            </a>
+
+            <a
+              href={project.homepage}
+              target="_blank"
+              className="rounded-full bg-transparent border-2 border-pink-700 text-pink-700 py-1 px-3 hover:bg-blue-600 hover:text-white dark:hover:text-black hover:border-white dark:hover:border-black transition-all duration-300 ease-in-out"
+              rel="noreferrer noopener"
+            >
+              Live Demo
+            </a>
+          </div>
         </div>
       </div>
     </div>
