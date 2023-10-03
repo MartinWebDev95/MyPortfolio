4c577a28821dadef363486850184806e7e6946c9
Juan Antonio <jamll.muro@gmail.com>
2023-10-03 14:12:44 +0200

    Add an animation to the SocialButtons component

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -2,16 +2,18 @@ import Link from './Link';
 
 function SocialButtons() {
   return (
-    <div className="flex items-center gap-4 md:gap-8 mx-auto mt-12">
-      <Link
-        text="Github"
-        href="https://github.com/MartinWebDev95"
-      />
+    <div className="md:gap-8 mx-auto mt-6 overflow-hidden p-4">
+      <div className="flex items-center gap-4 animate-socials_animation">
+        <Link
+          text="Github"
+          href="https://github.com/MartinWebDev95"
+        />
 
-      <Link
-        text="Linkedin"
-        href="https://github.com/MartinWebDev95"
-      />
+        <Link
+          text="Linkedin"
+          href="https://github.com/MartinWebDev95"
+        />
+      </div>
     </div>
   );
 }
