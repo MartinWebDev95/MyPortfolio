7abd4b002b2936a04cddcf82b05b14216d76957e
Juan Antonio <jamll.muro@gmail.com>
2023-10-03 13:28:00 +0200

    Add a new animation to the NavigationLink component

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -1,11 +1,31 @@
 function NavigationLink({ text }) {
   return (
-    <li className="overflow-hidden">
+    <li className="group relative cursor-pointer transition-all duration-300 ease-in-out">
       <a
         href="#"
-        className="text-black dark:text-white font-bold lg:text-xl hover:text-blue-600 dark:hover:text-blue-600 transition-all duration-300 ease-in-out"
+        className="flex items-center text-black dark:text-white font-bold lg:text-xl relative"
       >
-        {text}
+        {text.split('').map((letter, index) => (
+          <span
+            key={index}
+            className="origin-top transition-transform duration-300 ease-in-out group-hover:scale-y-0"
+            style={{ transitionDelay: `${index * 75}ms` }}
+          >
+            {letter}
+          </span>
+        ))}
+
+        <div className="flex items-center text-black dark:text-white font-bold lg:text-xl absolute bottom-0 left-0">
+          {text.split('').map((letter, index) => (
+            <span
+              key={index}
+              className="origin-bottom transition-transform duration-300 ease-in-out scale-y-0 group-hover:scale-y-100"
+              style={{ transitionDelay: `${index * 75}ms` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </div>
       </a>
     </li>
   );
