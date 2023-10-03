18042c9b701f6a38a0a54b4a2beea224f500a882
Juan Antonio <jamll.muro@gmail.com>
2023-10-03 11:44:54 +0200

    Change the color of the MenuMobile component

--- src/components/MenuMobile.jsx
+++ src/components/MenuMobile.jsx
@@ -5,9 +5,9 @@ function MenuMobile({ isMenuOpen, setIsMenuOpen }) {
       className="relative w-6 h-5 md:hidden flex"
       onClick={() => setIsMenuOpen(!isMenuOpen)}
     >
-      <span className={`absolute ${isMenuOpen ? 'rotate-45 top-2' : 'rotate-0 top-0'} left-0 w-6 h-1 bg-pink-700 rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
-      <span className={`absolute ${isMenuOpen ? 'w-0' : 'w-6'} h-1 bg-pink-700 top-2 left-0 rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
-      <span className={`absolute ${isMenuOpen ? '-rotate-45 top-2' : 'rotate-0 top-4'} w-6 h-1 bg-pink-700 left-0 rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
+      <span className={`absolute ${isMenuOpen ? 'rotate-45 top-2' : 'rotate-0 top-0'} left-0 w-6 h-1 bg-black dark:bg-white rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
+      <span className={`absolute ${isMenuOpen ? 'w-0' : 'w-6'} h-1 bg-black dark:bg-white top-2 left-0 rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
+      <span className={`absolute ${isMenuOpen ? '-rotate-45 top-2' : 'rotate-0 top-4'} w-6 h-1 bg-black dark:bg-white left-0 rounded-r-full rounded-l-full transition-all duration-300 ease-in-out`} />
     </button>
   );
 }
