a9104e83f1138a7c2259b82c27b7442f3b2e6002
Juan Antonio <jamll.muro@gmail.com>
2023-10-03 13:58:09 +0200

    Add a new Link component where the link href and the link name are passed by props

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -1,23 +1,17 @@
+import Link from './Link';
+
 function SocialButtons() {
   return (
-    <div className="flex items-center gap-4 md:gap-8 w-fit mx-auto mt-12 overflow-hidden">
-      <a
+    <div className="flex items-center gap-4 md:gap-8 mx-auto mt-12">
+      <Link
+        text="Github"
         href="https://github.com/MartinWebDev95"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="w-fit text-base md:text-lg dark:text-white text-black dark:border-white border-black border-2 rounded-xl px-4 py-1 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black font-bold transition-colors ease-in-out duration-300 animate-socials_animation"
-      >
-        Github
-      </a>
+      />
 
-      <a
+      <Link
+        text="Linkedin"
         href="https://github.com/MartinWebDev95"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="w-fit text-base md:text-lg dark:text-white text-black dark:border-white border-black border-2 rounded-xl px-4 py-1 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black font-bold transition-colors ease-in-out duration-300 animate-socials_animation"
-      >
-        Linkedin
-      </a>
+      />
     </div>
   );
 }
