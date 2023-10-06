8a9c3a016346e1160d7797cd12afd6a324423e16
Juan Antonio <jamll.muro@gmail.com>
2023-10-06 12:53:20 +0200

    Change some styles in Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,13 +4,13 @@ function Link({ text, href }) {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="group relative font-bold text-lg text-black dark:text-white hover:bg-black hover:text-white hover:dark:bg-white hover:dark:text-black rounded-lg border-2 dark:border-white border-black px-4 py-1 hover:scale-110 transition-all ease-in-out duration-300"
+      className="group/link relative font-bold text-lg text-black dark:text-white hover:bg-black hover:text-white hover:dark:bg-white hover:dark:text-black rounded-lg border-2 dark:border-white border-black px-4 py-1 hover:scale-110 transition-all ease-in-out duration-300"
     >
       <div className="flex items-center relative">
         {text.split('').map((letter, index) => (
           <span
             key={index}
-            className="origin-top transition-transform duration-300 ease-in-out group-hover:scale-y-0"
+            className="origin-top transition-transform duration-300 ease-in-out group-hover/link:scale-y-0"
             style={{ transitionDelay: `${index * 75}ms` }}
           >
             {letter}
@@ -21,7 +21,7 @@ function Link({ text, href }) {
           {text.split('').map((letter, index) => (
             <span
               key={index}
-              className="origin-bottom transition-transform duration-300 ease-in-out scale-y-0 group-hover:scale-y-100"
+              className="origin-bottom transition-transform duration-300 ease-in-out scale-y-0 group-hover/link:scale-y-100"
               style={{ transitionDelay: `${index * 75}ms` }}
             >
               {letter}
