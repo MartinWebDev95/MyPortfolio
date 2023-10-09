368f64ddfd281ae5e79f479d678fd68ebad4c152
Juan Antonio <jamll.muro@gmail.com>
2023-10-09 12:25:40 +0200

    Remove some components to Portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,9 +1,7 @@
 import Blobs from './components/Blobs';
-import ChangeLanguage from './components/ChangeLanguage';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import SectionProjects from './components/SectionProjects';
-import SectionSkills from './components/SectionSkills';
 
 function Portfolio() {
   return (
@@ -12,15 +10,11 @@ function Portfolio() {
 
       <Header />
 
-      <main className="container mx-auto px-4 md:px-0">
+      <main>
         <HeroSection />
 
         <SectionProjects />
-
-        <SectionSkills />
       </main>
-
-      <ChangeLanguage />
     </>
   );
 }
