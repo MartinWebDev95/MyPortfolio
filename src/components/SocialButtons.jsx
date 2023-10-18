73f2d53cacac62f96da2027d17f86c635439a987
Juan Antonio <jamll.muro@gmail.com>
2023-10-18 14:05:38 +0200

    Add the WebLink component into SocialButtons component

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -1,15 +1,15 @@
-import Link from './Link';
+import WebLink from './WebLink';
 
 function SocialButtons() {
   return (
     <div className="md:gap-8 mx-auto mt-6 overflow-hidden p-4">
       <div className="flex items-center gap-4 animate-socials_animation">
-        <Link
+        <WebLink
           text="Github"
           href="https://github.com/MartinWebDev95"
         />
 
-        <Link
+        <WebLink
           text="Linkedin"
           href="https://github.com/MartinWebDev95"
         />
