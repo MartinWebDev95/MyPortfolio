f3bd2f953f233eebd2ca3711568d59080e84c851
MartinWebDev95 <jamll.muro@gmail.com>
2023-11-02 11:34:28 +0100

    Change some styles in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   const { isScrolled } = useOpacity();
 
   return (
-    <header className={`w-full ${(isMenuOpen || isScrolled) ? 'bg-white/60 dark:bg-black/60' : 'bg-white/20 dark:bg-black/20'} ${(isMenuOpen) && 'bg-white/95 dark:bg-black/95'} backdrop-blur-lg h-20 z-30 flex sticky top-0 md:shadow-lg`}>
+    <header className={`w-full ${isScrolled ? 'bg-white/60 dark:bg-black/60' : 'bg-white/20 dark:bg-black/20'} ${(isMenuOpen) && 'bg-white/95 dark:bg-black/95 md:bg-transparent md:dark:bg-transparent'} backdrop-blur-lg h-20 z-30 flex sticky top-0 md:shadow-lg`}>
       <div className="flex justify-between items-center container mx-auto px-4 md:px-0">
         <MenuMobile isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
