c04e527dde867345b67fee067306ccc1196778d5
Juan Antonio <jamll.muro@gmail.com>
2023-11-22 10:02:28 +0100

    Change the InputForm component to show the form errors if they exists

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,18 +1,27 @@
 function InputForm({
-  label, type, placeholder, name,
+  label, type, placeholder, name, formValues, setFormValues, error, setFormErrors,
 }) {
   return (
     <label className="flex flex-col gap-2" htmlFor={name}>
-      <span className="text-black dark:text-white font-bold">
-        {label}
-      </span>
+      <p className={`${error !== '' ? 'text-red-600' : 'text-black dark:text-white'} font-bold flex items-center justify-between`}>
+        <span>
+          {label}
+        </span>
+
+        <span className={`${error !== '' ? 'block' : 'hidden'}`}>
+          {error}
+        </span>
+      </p>
 
       <input
         type={type}
         name={name}
         id={name}
+        value={formValues[name]}
         className="rounded-md border-2 border-white dark:border-black bg-black dark:bg-white text-white dark:text-black p-1 px-2 placeholder:dark:text-black/70"
         placeholder={placeholder}
+        onFocus={(prevState) => setFormErrors({ ...prevState, [name]: '' })}
+        onChange={(e) => setFormValues({ ...formValues, [name]: e.target.value })}
       />
     </label>
   );
