9013c895f5f58cec0d51721fb6c92d365ff1b654
Juan Antonio <jamll.muro@gmail.com>
2023-11-28 10:51:07 +0100

    Add two translations files with the web content in spanish and english

---
+++ src/translations/en/global.js
@@ -0,0 +1,51 @@
+const globalEn = {
+  language: 'Choose the language: ',
+  navigation: {
+    firstItem: 'Home',
+    secondItem: 'Projects',
+    thirdItem: 'Skills',
+    fourthItem: 'Contact',
+  },
+  heroSection: {
+    title: 'Hi, I\'m a Junior Frontend Developer',
+    subtitle: 'My name is Juan Antonio Martín and I\'m looking for a job as frontend developer',
+  },
+  projects: {
+    title: 'Projects',
+  },
+  skills: {
+    title: 'Skills',
+  },
+  contact: {
+    title: 'Contact',
+    error: 'Field cannot be empty',
+    nameInput: {
+      label: 'Name:',
+      error: 'This is not a correct name',
+      placeholder: 'Your name',
+    },
+    emailInput: {
+      label: 'Email:',
+      error: 'This is not a correct email',
+      placeholder: 'Your email',
+    },
+    topicInput: {
+      label: 'Topic:',
+      error: 'This is not a correct topic',
+      placeholder: 'Your topic',
+    },
+    messageInput: {
+      label: 'Message:',
+      error: 'This is not a correct message',
+      placeholder: 'Your message',
+    },
+    buttonTitle: 'Send',
+  },
+  notification: {
+    loading: 'Sending email...',
+    success: 'Email sent',
+    error: 'The email couldn&apos;t be sent',
+  },
+};
+
+export default globalEn;
---
+++ src/translations/es/global.js
@@ -0,0 +1,51 @@
+const globalEs = {
+  language: 'Cambiar idioma: ',
+  navigation: {
+    firstItem: 'Inicio',
+    secondItem: 'Proyectos',
+    thirdItem: 'Habilidades',
+    fourthItem: 'Contacto',
+  },
+  heroSection: {
+    title: 'Hola, soy un Desarrollador Web Junior Frontend',
+    subtitle: 'Mi nombre es Juan Antonio Martín y estoy buscando un trabajo como desarrollador frontend',
+  },
+  projects: {
+    title: 'Proyectos',
+  },
+  skills: {
+    title: 'Habilidades',
+  },
+  contact: {
+    title: 'Contacto',
+    error: 'El campo no puede estar vacío',
+    nameInput: {
+      label: 'Nombre:',
+      error: 'No es un nombre válido',
+      placeholder: 'Su nombre',
+    },
+    emailInput: {
+      label: 'Email:',
+      error: 'No es un email válido',
+      placeholder: 'Su email',
+    },
+    topicInput: {
+      label: 'Asunto:',
+      error: 'No es un asunto válido',
+      placeholder: 'Su asunto',
+    },
+    messageInput: {
+      label: 'Mensaje:',
+      error: 'No es una mensaje válido',
+      placeholder: 'Su mensaje',
+    },
+    buttonTitle: 'Enviar',
+  },
+  notification: {
+    loading: 'Enviando email...',
+    success: 'Email enviado',
+    error: 'El email no pudo ser enviado',
+  },
+};
+
+export default globalEs;
